d9a656073305776531b0171c26059286a39e6afb
s.corderoc <s.corderoc@jardinazuayo.fin.ec>
2023-10-14 10:23:59 -0500

    Create interface.js

---
+++ components/representante/interface.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const response = require('../../network/response')
+const controller = require('./controller')
+
+const route = express.Router()
+
+route.get('/', function(req, res) {
+    const filtro_representante = req.query.codigo || null
+    controller.get_representante( filtro_representante )
+        .then( (data) => response.success(req, res, data, 200) )
+        .catch( (error) => response.error(req, res, error, 500) )
+})
+
+route.post('/', function(req, res) {
+    controller.add_representante( req.body )
+        .then( (data) => response.success(req, res, data, 201) )
+        .catch( (error) => response.error(req, res, error, 500) )
+})
+
+route.patch('/', function(req, res) {
+    controller.update_representante( req.body )
+        .then( (data) => response.success(req, res, data, 200) )
+        .catch( (error) => response.error(req, res, error, 500) )
+})
+
+module.exports = route
